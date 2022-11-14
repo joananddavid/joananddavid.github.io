b978f13ac9f273dda3de077417d802820a83ed96
David Atkinson <davatk@gmail.com>
2022-11-13 20:11:14 -0700

    manual html

---
+++ assets/js/globals.js
@@ -0,0 +1,5 @@
+var use_rsvp_code = true;
+var rsvp_code_md5 = '81dc9bdb52d04dc20036dbd8313ed055';
+var rsvp_post_address = 'https://api.nathanorick.com/rsvp';
+var api_key = 'm7ccqIIa6S8uSBQzbhZzd1rZaoiCXObr1fcx4Wvy';
+
